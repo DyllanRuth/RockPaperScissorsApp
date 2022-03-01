b63b1bdbd6e0998b1a3b7e9aff73e41bfbfca3ce
DyllanRuth <73084302+DyllanRuth@users.noreply.github.com>
2022-03-01 15:43:14 -0600

    App Template

---
+++ druth_proj4/druth_proj4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  druth_proj4
+//
+//  Created by STUDENT-SP22 on 2/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
